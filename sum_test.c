3182d0081665706ddcaa4142f80bef96dfbcc232
Isaac Lyu <lyuid@vcu.edu>
2023-10-30 14:33:57 -0400

    sum program

--- sum_test.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "sum.c"
-
-int main()
-{
-printf("This is the sum of 1 and 2");
-printf("%d", sum(1,2));
-}
--- sum_test2.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "sum.c"
-
-int main()
-{
-printf("This is the sum of 0 and 12");
-printf("%d", sum(0,12));
-}
